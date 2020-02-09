31aef48608e89cd049caad8791fbc6281be8fdef
Meowth Camila <mcamila@mi-a4.21-school.ru>
2020-02-09 18:52:27 +0300

    Mother Earth first commit

---
+++ Mother Earth/Assets/Scripts/Controllers/Executor.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Executor : MonoBehaviour
+{
+    private BasicController[] controllers;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Найти все контроллеры
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        foreach (var controller in controllers)
+        {
+            controller.Execute();
+        }
+    }
+}
