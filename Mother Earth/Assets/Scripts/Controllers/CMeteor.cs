eca9875f3f07cf614d08861515a50d31346d1667
lyahasik <lyahasik@yandex.ru>
2020-02-12 16:31:21 +0300

    CMeteor
    Спаун, движение, столкновение метеоритов.

---
+++ Mother Earth/Assets/Scripts/Controllers/CMeteor.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CMeteor : BasicController
+{
+    public GameObject Meteor;
+    public GameObject Explosion;
+    public float MinDelaySpawn = 1.0f;
+    public float MaxDelaySpawn = 2.0f;
+    public float SpeedMove = 1.0f;
+    public float SpeedRotate = 100.0f;
+    private GameObject mainCamera;
+    private float nextSpawn;
+    private GameObject[] hexes;
+    public List<MeteorData> meteors;
+    private void Start()
+    {
+        mainCamera = GameObject.FindGameObjectWithTag("MainCamera");
+        meteors = new List<MeteorData>();
+        hexes = GameObject.FindGameObjectsWithTag("Hex");
+        nextSpawn = Random.Range(MinDelaySpawn, MaxDelaySpawn);
+    }
+
+    public override void Execute()
+    {
+        if (base.IsRun())
+        {
+            if (nextSpawn <= 0.0f)
+            {
+                meteors.Add(spawn());
+                nextSpawn = Random.Range(MinDelaySpawn, MaxDelaySpawn);
+            }
+            else
+            {
+                nextSpawn -= Time.deltaTime;
+            }
+            Invoke("step", 0.1f);
+        }
+    }
+
+    private MeteorData spawn()
+    {
+        GameObject prefab;
+        MeteorData data;
+
+        prefab = Instantiate(
+            Meteor,
+            mainCamera.transform.position,
+            Quaternion.identity);
+        data = prefab.GetComponent<MeteorData>();
+        data.GetDirectionMove(data.SetDirectionMove() * SpeedMove);
+        data.GetRotateMove(data.SetRotateMove() * Random.Range(1.0f, SpeedRotate));
+        return data;
+    }
+
+    private void step()
+    {
+        foreach (MeteorData data in meteors)
+        {
+            data.SetPrefab().transform.position += data.SetDirectionMove() * Time.deltaTime * SpeedMove;
+            data.SetMeteor().transform.rotation *= Quaternion.Euler(
+                data.SetRotateMove().x * Time.deltaTime * SpeedRotate,
+                data.SetRotateMove().y * Time.deltaTime * SpeedRotate,
+                data.SetRotateMove().z * Time.deltaTime * SpeedRotate);
+        }
+    }
+
+    public void Collision(GameObject meteor, GameObject Hex)
+    {
+        //Hex.GetComponent<name>().name;
+        Instantiate(Explosion, meteor.transform.position, Quaternion.identity);
+        meteors.Remove(meteor.GetComponent<MeteorData>());
+        Destroy(meteor);
+    }
+}
---
+++ Mother Earth/Assets/Scripts/Meteor/MeteorData.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MeteorData : MonoBehaviour
+{
+    private GameObject mainCamera;
+    private GameObject prefab;
+    private Vector3 directionMove;
+    private Vector3 rotateMove;
+    private GameObject meteor;
+
+    private void Start()
+    {
+        meteor = transform.Find("Meteor").gameObject;
+        mainCamera = GameObject.FindGameObjectWithTag("MainCamera");
+        transform.RotateAround(Vector3.zero, mainCamera.transform.right, Random.Range(10.0f, 90.0f));
+        transform.RotateAround(Vector3.zero, mainCamera.transform.forward, Random.Range(0.0f, 360.0f));
+        prefab = gameObject;
+        directionMove = Vector3.Normalize(Vector3.zero - transform.position);
+        rotateMove = Random.insideUnitSphere;
+    }
+
+    public GameObject SetPrefab()
+    {
+        return prefab;
+    }
+
+    public GameObject SetMeteor()
+    {
+        return meteor;
+    }
+
+    public void GetDirectionMove(Vector3 direction)
+    {
+        directionMove = direction;
+    }
+    public Vector3 SetDirectionMove()
+    {
+        return directionMove;
+    }
+
+    public void GetRotateMove(Vector3 rotate)
+    {
+        rotateMove = rotate;
+    }
+    public Vector3 SetRotateMove()
+    {
+        return rotateMove;
+    }
+}
